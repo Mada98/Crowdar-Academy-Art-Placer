685f0b0ba54ba7040c46687f156d2a2c30b698b8
Ramiro Gabeiras <ramirogabeiras1998@gmail.com>
2026-03-13 16:51:43 -0300

    com

--- Art Placer/src/main/java/lippia/web/constants/LoginConstants.java
+++ Art Placer/src/main/java/lippia/web/constants/LoginConstants.java
@@ -5,5 +5,5 @@ public class LoginConstants {
     public static final String INPUT_PASSWORD = "name:password";
     public static final String BUTTON_LOGIN = "xpath://input[@type='submit' and @value='login']";
     public static final String STRING_TITLE_XPATH = "id:welcomeName";
-    public static final String INPUT_FORGOT_EMAIL = "id:email-recovery";
+    public static final String INPUT_FORGOT_EMAIL = "name:email";
 }
